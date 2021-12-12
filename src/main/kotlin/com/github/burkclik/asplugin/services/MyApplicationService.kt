aebf3ff0c11b1aceec78297725c8b6821d200e85
GitHub Action <action@github.com>
2021-12-12 17:44:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/burkclik/asplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/burkclik/asplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.burkclik.asplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.burkclik.asplugin.MyBundle
 
 class MyApplicationService {
 
