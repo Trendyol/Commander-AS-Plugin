713eced9833d837bf49c3cb7243a8b14b84aab9f
Ayyüce Kurt <ayyuce.kurt@trendyol.com>
2022-06-07 13:34:49 +0300

    add group action

---
+++ src/main/kotlin/com/github/burkclik/asplugin/actions/RunGradleAction.kt
@@ -0,0 +1,11 @@
+package com.github.burkclik.asplugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class RunGradleAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
