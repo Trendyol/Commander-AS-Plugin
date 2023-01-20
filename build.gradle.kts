20bfcae2d48e217440318dc0dafad1ab10357218
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2023-01-20 16:07:10 +0100

    Update java toolchain version for kotlin java related tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -132,11 +132,4 @@ tasks {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
+kotlin.jvmToolchain(11)
