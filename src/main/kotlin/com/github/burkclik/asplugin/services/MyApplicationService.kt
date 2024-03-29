ea9ae6268f4e5f44d9706787f3f13d3be9d87b3f
burak.celik1 <burak.celik1@trendyol.com>
2024-03-29 13:11:09 +0300

    PLUGIN-0 | add script reader  - add support for Android Studio Iguana

--- src/main/kotlin/com/github/burkclik/asplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/burkclik/asplugin/services/MyApplicationService.kt
@@ -1,7 +1,9 @@
 package com.github.burkclik.asplugin.services
 
 import com.github.burkclik.asplugin.MyBundle
+import com.intellij.openapi.components.Service
 
+@Service
 class MyApplicationService {
 
     init {
